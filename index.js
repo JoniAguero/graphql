02f966681ba64546a6f4832b7a421c5a9c437d26
JoniAguero <jaguero@trimix.com.ar>
2018-11-11 19:28:04 -0300

    Schema
    Se crea un simple schema de GraphQL, y se configura apollo server

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
-const express = require('express');
+const express = require('express')
+const { ApolloServer } = require('apollo-server-express')
 
-const app = express();
-const PORT = 4000;
-app.listen(PORT, () => {
-    console.log('Server ready at http://localhost:4000');
-    
+// construye un schema, usando GraphQL
+const typeDefs = require('./schema')
+
+// provee resolvers para nuestro esquema
+const resolvers = {
+    Query: {
+        cursos: () => []
+    }
+}
+
+// inicializar apollo server
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
 })
+
+// inicializar express
+const app = express()
+
+// aplicar middleware de apollo en express
+server.applyMiddleware({
+    app
+})
+
+// iniciar el servidor
+app.listen({
+        port: 4000
+    }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+)
