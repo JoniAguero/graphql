55c7aaba74cd0b987617dca2a97587f26ae90928
JoniAguero <jaguero@trimix.com.ar>
2018-11-12 10:53:42 -0300

    Conectado
    Se conecta Client en React con GraphQL

--- react-client-graphql/src/App.js
+++ react-client-graphql/src/App.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import gql from "graphql-tag";
+import { graphql } from 'react-apollo';
+
+
 class App extends Component {
+
   render() {
+    console.log(this.props);
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="App">
+          
+        </div>
     );
   }
 }
 
-export default App;
+const CursoQuery = gql`
+  query CursoQuery {
+    cursos {
+      titulo
+      descripcion
+      profesor {
+        nombre
+      }
+    }
+  }`
+
+export default graphql(CursoQuery)(App);
